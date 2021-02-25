a5ebb30e95695d6cefce62a451203623c423cd2a
misshia <73309344+misshia@users.noreply.github.com>
2021-02-25 17:52:00 +0100

    weather js added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './App.css';
-
 import Footer from "./Footer"
-function App() {
-  return (
-    <div className="App">
+
+export default function App() {
+  return <div className="App">
+    <div className="container">
       <h1>weather cat react app</h1> 
         <Footer />
-    </div>
-  );
+        </div>
+    </div>;
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,2 @@
+import React from "react";
+import weather from "./weather.css";
