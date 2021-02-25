11aa8a264f5ac177773ad6a08c95fdfdfcec5b60
misshia <73309344+misshia@users.noreply.github.com>
2021-02-25 10:33:47 +0100

    Links added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      weather cat react app 
-        <p>
-          codded by misia
-        </p>
+      <h1>weather cat react app</h1> 
+        <footer>
+          codded by {" "}
         <a
-          href="https://reactjs.org"
+          href="https://pl.linkedin.com/in/maria-kozielska-5018751bbreactjs.org"
           target="_blank"
           rel="noreferrer"
-        >
-          open sourced on github
+        > 
+        misia
         </a>
-      
+          open source on github
+          <a 
+          href= "https://github.com/misshia/my-app-weather"
+          target="_blank"
+          rel="noreferrer">
+        </a>
+      </footer>
     </div>
   );
 }
