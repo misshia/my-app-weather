00de1e6d6856298acb9dd7b10eaaf60f833ce8ea
misshia <73309344+misshia@users.noreply.github.com>
2021-02-26 22:54:35 +0100

    temperature size changed

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather () {
 
       <div className="temp">
             <div className= "element">
-              <h4><span id ="current-temperature">18</span>°</h4>
+            <span id ="current-temperature">18</span>°
             <div className="btn-group-vertical">
               <div className="buttonC" id="celsius" click="convertToCelsius()" >
                 C
